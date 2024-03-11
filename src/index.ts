8da43839423b633fbe71abf4e43d5845e2702510
Vivek <kumarrawanivivek@gmail.com>
2024-03-11 22:48:07 +0530

    auth implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { Context } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const authMiddleware =  async (c : Context, next : any) => {
+  const condition = false;
+  if(condition) {
+      await next();
+  }else {
+      console.log("Not authorized!");
+      return c.json({message : "Oops! This is not allowed!"}, 403)
+  }
+  
+}
 
-app.post('/post', (c)=>{
-  return c.json({ message : "Hello! this is post request"})
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/", authMiddleware, async (c) => {
+  const header = c.req.header("User-Agent");
+  const qrs = c.req.queries();
+  const id = c.req.query("id");
+  const body = await c.req.json();
+  console.log("id :", id);
+
+  console.log("header : ",header);
+  console.log("queries -> ", qrs)
+  return c.json({
+    message: "Hello! this is post request",
+    header,
+    id,
+    body,
+    qrs,
+  });
+});
+
+export default app;
