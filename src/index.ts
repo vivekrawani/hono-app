5b8450b1cd1aa65eac591aff69ddff193e6ef06c
Vivek <kumarrawanivivek@gmail.com>
2024-03-11 23:23:57 +0530

    ,

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { Context } from "hono";
 const app = new Hono();
 
 const authMiddleware =  async (c : Context, next : any) => {
-  const condition = false;
+  const milli = new Date().getTime();
+  const ten_seconds = Math.floor((milli / 1000 ) / 10);
+  const condition = (ten_seconds % 2 == 0);
   if(condition) {
       await next();
   }else {
       console.log("Not authorized!");
-      return c.json({message : "Oops! This is not allowed!"}, 403)
+      return c.json({message : "Can't handle the request now"}, 403)
   }
   
 }
@@ -19,21 +21,26 @@ app.get("/", (c) => {
 });
 
 app.post("/", authMiddleware, async (c) => {
-  const header = c.req.header("User-Agent");
-  const qrs = c.req.queries();
-  const id = c.req.query("id");
-  const body = await c.req.json();
-  console.log("id :", id);
-
-  console.log("header : ",header);
-  console.log("queries -> ", qrs)
-  return c.json({
-    message: "Hello! this is post request",
-    header,
-    id,
-    body,
-    qrs,
-  });
+  try {
+    const header = c.req.header("User-Agent");
+    const qrs = c.req.queries();
+    const id = c.req.query("id");
+    const body = await c.req.json();
+    console.log("id :", id);
+  
+    console.log("header : ",header);
+    console.log("queries -> ", qrs)
+    return c.json({
+      message: "Hello! this is post request",
+      header,
+      id,
+      body,
+      qrs,
+    });
+  } catch (error) {
+    return c.json({message : "Bad Request bro!"}, 400)
+  }
+ 
 });
 
 export default app;
